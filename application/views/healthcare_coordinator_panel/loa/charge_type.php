ff56cbeb1ba4ec24fd151e155347e5a07a19a671
cajegasjendev <jenprog08@gmail.com>
2023-03-10 11:56:56 +0800

    charge type update

--- application/views/healthcare_coordinator_panel/loa/charge_type.php
+++ application/views/healthcare_coordinator_panel/loa/charge_type.php
@@ -13,7 +13,7 @@
                     <div class="row mb-3">
                       <input type="hidden" name="token" value="<?= $this->security->get_csrf_hash() ?>">
                       <input type="hidden" name="loa-id" id="loa-id">
-                      <select class="chargetype" name="charge-type">
+                      <select class="chargetype fs-5" name="charge-type">
                         <option value="">Please Select...</option>
                         <option value="Yes">Work related</option>
                         <option value="No">Non-work related</option>
@@ -22,8 +22,8 @@
 		                  <div class="form-group row pt-4">
 		                    <div class="wr" id="percentage">
 		                      <div id="med-services-wrapper">
-		                        <div class="mb-2">
-		                          <label class="colored-label">Enter Percentage (work related)</label>
+		                        <div class="mb-2 fs-5">
+		                          <label class="colored-label">Enter Percentage (work-related)</label>
 		                          <input type="number" class="form-control" name="wr_percentage">
 		                        </div>
 		                      </div>
@@ -34,8 +34,8 @@
 	                    <div class="form-group row">
 	                      <div class="nwr" id="percentage">
 	                        <div id="med-services-wrapper">
-	                          <div class="mb-2">
-	                            <label class="colored-label">Enter Percentage (non-work related)</label>
+	                          <div class="mb-2 fs-5">
+	                            <label class="colored-label">Enter Percentage (nonwork-related)</label>
 	                            <input type="number" class="form-control" name="nwr_percentage">
 	                          </div>
 	                        </div>
@@ -65,14 +65,13 @@
     $('.chargetype').on('change',function(){
       var value = $(this).val();
       if(value == "Yes"){
-        $( ".wr" ).hide();
-        $( ".nwr" ).show();
-
-      }else if(value == "No"){
         $( ".wr" ).show();
         $( ".nwr" ).hide();
-      }
-      else if(value == ""){
+
+      }else if(value == "No"){
+        $( ".wr" ).hide();
+        $( ".nwr" ).show();
+      }else if(value == ""){
         $( ".wr" ).hide();
         $( ".nwr" ).hide();
       }

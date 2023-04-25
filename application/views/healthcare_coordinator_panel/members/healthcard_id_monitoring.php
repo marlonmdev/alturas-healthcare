6ce96e96f1de89d7b3a86b577161353dcd51bf78
liniefrancisco <liniefrancisco01@gmail.com>
2023-04-25 13:41:14 +0800

    Update member in coordinator

--- application/views/healthcare_coordinator_panel/members/healthcard_id_monitoring.php
+++ application/views/healthcare_coordinator_panel/members/healthcard_id_monitoring.php
@@ -1,93 +1,81 @@
-  <!-- Start of Page Wrapper -->
-  <div class="page-wrapper">
-    <!-- Bread crumb and right sidebar toggle -->
-    <div class="page-breadcrumb">
-      <div class="row">
-        <div class="col-12 d-flex no-block align-items-center">
-          <h4 class="page-title ls-2">Members</h4>
-          <div class="ms-auto text-end">
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item">Healthcare Coordinator</li>
-                <li class="breadcrumb-item active" aria-current="page">
-                  Approved
-                </li>
-              </ol>
-            </nav>
-          </div>
+<div class="page-wrapper">
+  <div class="page-breadcrumb">
+    <div class="row">
+      <div class="col-12 d-flex no-block align-items-center">
+        <h4 class="page-title ls-2">HEALTHCARE MEMBER</h4>
+        <div class="ms-auto text-end">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">Healthcare Coordinator</li>
+              <li class="breadcrumb-item active" aria-current="page">max benefit limit</li>
+            </ol>
+          </nav>
         </div>
       </div>
     </div>
-    <!-- End Bread crumb and right sidebar toggle -->
-    <!-- Start of Container fluid  -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <ul class="nav nav-tabs mb-4" role="tablist">
-                    <li class="nav-item">
-                    <a
-                        class="nav-link"
-                        href="<?php echo base_url(); ?>healthcare-coordinator/members"
-                        role="tab"
-                        ><span class="hidden-sm-up"></span>
-                        <span class="hidden-xs-down fs-5 font-bold">Pending</span></a
-                    >
-                    </li>
-                    <li class="nav-item">
-                    <a
-                        class="nav-link"
-                        href="<?php echo base_url(); ?>healthcare-coordinator/members/approved"
-                        role="tab"
-                        ><span class="hidden-sm-up"></span>
-                        <span class="hidden-xs-down fs-5 font-bold">Approved</span></a
-                    >
-                    </li>
-                    <li class="nav-item">
-                    <a
-                        class="nav-link active"
-                        href="<?php echo base_url(); ?>healthcare-coordinator/members/approved/uploaded-scanned-id-form"
-                        role="tab"
-                        ><span class="hidden-sm-up"></span>
-                        <span class="hidden-xs-down fs-5 font-bold">Healthcard ID Monitoring</span></a
-                    >
-                    </li>
-                </ul>
+  </div>
 
-                <div class="card shadow">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-hover table-responsive" id="doneHcIdMembersTable">
-                                <thead>
-                                    <tr>
-                                        <th class="fw-bold">#</th>
-                                        <th class="fw-bold">Name</th>
-                                        <th class="fw-bold">EmpType</th>
-                                        <th class="fw-bold">Status</th>
-                                        <th class="fw-bold">Business Unit</th>
-                                        <th class="fw-bold">Department</th>
-                                        <th class="fw-bold">ID Status</th>
-                                        <th class="fw-bold">Scanned ID</th>
-                                        <th class="fw-bold">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
 
-                </div>
+        <ul class="nav nav-tabs mb-4" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url(); ?>healthcare-coordinator/members" role="tab">
+              <span class="hidden-sm-up"></span>
+              <span class="hidden-xs-down fs-5 font-bold">CREATE ACOOUNT</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url(); ?>healthcare-coordinator/members/approved" role="tab">
+              <span class="hidden-sm-up"></span>
+              <span class="hidden-xs-down fs-5 font-bold">UPLOAD HEALTHCARD ID</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link active" href="<?php echo base_url(); ?>healthcare-coordinator/members/approved/uploaded-scanned-id-form" role="tab">
+              <span class="hidden-sm-up"></span>
+              <span class="hidden-xs-down fs-5 font-bold">MBL MONITORING</span>
+            </a>
+          </li>
+        </ul>
+
+        <div class="card shadow">
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table table-hover table-responsive" id="doneHcIdMembersTable">
+                <thead>
+                  <tr>
+                    <th class="fw-bold">#</th>
+                    <th class="fw-bold">NAME OF EMPLOYEE</th>
+                    <th class="fw-bold">TYPE OF EMPLOYEE</th>
+                    <th class="fw-bold">STATUS</th>
+                    <th class="fw-bold">BUSINESS UNIT</th>
+                    <th class="fw-bold">DEPARTMENT</th>
+                    <th class="fw-bold">STATUS</th>
+                    <th class="fw-bold">HEALTHCARD ID</th>
+                    <th class="fw-bold">ACTION</th>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </table>
             </div>
-       <!-- End Row  -->  
+          </div>
+        </div>
+
       </div>
-    <!-- End Container fluid  -->
     </div>
-  <!-- End Page wrapper  -->
   </div>
   <?php include 'view_healthcard_id.php' ?>
-<!-- End Wrapper -->
 </div>
+
+  
+
+
+
 <script>
     const baseUrl = '<?php echo base_url(); ?>';
     $(document).ready(function(){
